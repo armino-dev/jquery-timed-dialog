c5f1cfd2778d239c8ed02dd5b45ceba144e34a58
Armino Popp <devel@boioiong.com>
2024-02-03 11:56:22 +0200

    Patch release (#222)
    * Remove webpack and add esbuild
    
    * Release 1.1.0

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-        entry: './src/timed-dialog.js',
-        output: {
-            filename: 'timed-dialog.min.js',
-            path: path.resolve(__dirname, 'dist'),
-        }
-};
