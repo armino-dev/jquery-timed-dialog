0c64f83e6ded444f029ed7db8bb51261608ddccf
Armino Popp <devel@boioiong.com>
2024-02-04 15:30:43 +0200

    Patch (#227)
    * Add support for dark theme
    
    * Release 1.1.4

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,15 +1,17 @@
 import * as esbuild from 'esbuild';
 import { sassPlugin } from "esbuild-sass-plugin";
+import fileSystem from 'fs';
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
 
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
+const WATCH = process.argv.includes('--watch');
 
 console.time('Build time');
 
-try {
+const buildCss = async () => {
     await esbuild.build({
-        entryPoints: ['scss/timed-dialog.scss'],
+        entryPoints: ['src/scss/timed-dialog.scss'],
         outfile: 'dist/timed-dialog.min.css',
         bundle: true,
         minify: true,
@@ -29,6 +31,9 @@ try {
             'process.env.NODE_ENV': `"${ENVIRONMENT}"`,
         },
     });
+}
+
+const buildJs = async () => {
     await esbuild.build({
         entryPoints: ['src/timed-dialog.js'],
         bundle: true,
@@ -40,8 +45,37 @@ try {
             'process.env.NODE_ENV': `"${ENVIRONMENT}"`,
         }
     });
+}
+
+try {
+    buildCss();
+    buildJs();
 } catch (e) {
     console.error(e);
 }
 
 console.timeEnd('Build time');
+
+if (WATCH) {
+    console.log('Watching for changes...');
+    console.log();
+
+    const watcher = fileSystem.watch('src', { recursive: true }, async (eventType, filename) => {
+        if (filename.includes('scss')) {
+            console.log(`${new Date().toLocaleTimeString()} Recompiling css ...`);
+            await buildCss();
+        } else if (filename.includes('js')) {
+            console.log(`${new Date().toLocaleTimeString()} Recompiling javascript ...`);
+            await buildJs();
+        }
+
+        if (filename.includes('scss') || filename.includes('js')) {
+            console.log(`${new Date().toLocaleTimeString()} Done`);
+        }
+    });
+
+    process.on('SIGINT', () => {
+        watcher.close();
+        process.exit();
+    });
+}
